8f8773fe3ffba84402ac3b12f744873b6f2f9eb1
dheerajsai chava <dheerajsaichava@gmail.com>
2019-09-10 19:48:13 -0400

    Add project files.

---
+++ conditional statements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace conditional_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("How many years experience do you have in professional programming?");
+                int years = int.Parse(Console.ReadLine());
+                if (years == 0)
+                    Console.WriteLine("needs lot of practise");
+                else if (years == 1)
+                    Console.WriteLine("looks likeu have a little experience");
+                else if (years == 2)
+                    Console.WriteLine("wow! You've been doing this for a little while");
+                else
+                    Console.WriteLine("you are a expert");
+            }
+            catch
+            {
+                Console.WriteLine("please enter an integer");
+            }
+        }
+    }
+}
